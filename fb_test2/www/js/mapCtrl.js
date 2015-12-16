c8db38c7e9649680bf30152987a506a7206d7c90
AnitaTsai <momo19940901@gmail.com>
2015-12-16 17:18:22 +0800

    fix bug

--- fb_test2/www/js/mapCtrl.js
+++ fb_test2/www/js/mapCtrl.js
@@ -139,34 +139,6 @@ angular.module('starter.mapCtrl', [] )
     };
     $scope.isActive = function(type) {
         return type === $scope.active;
-    };
-
-   $scope.gethistory = function() {
-     //alert('trigger gethistory()');
-    var MessageObject = Parse.Object.extend("MessageObject");
-    var query = new  Parse.Query(MessageObject);
- 
-    query.equalTo("username", $scope.rslt);
-    query.find({
-      success: function(results) {
-       // alert("Successfully retrieved " + results.length + " msgs.");
-      
-        $scope.msgs = [];
-        for (var i = 0; i < results.length; i++) {
-          var object = results[i];
-          //alert(object.id + ' - ' + object.get('message'));
-        // $scope.mes = object.get('message');
-         //console.log(results[i].get('message'));
-          
-            
-            $scope.msgs.push(results[i].get('message') );
-           
-        }
-      },
-      error: function(error) {
-       alert("Error: " + error.code + " " + error.message);
-      }
-    });
-  };
+    };  
 
 });
