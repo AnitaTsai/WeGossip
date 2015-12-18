d6b920ddaecf26e5bb3cad7d4809cd66e39d12f3
jerry900287 <jerry900287@gmail.com>
2015-12-19 00:09:32 +0800

    20151219

--- fb_test2/www/js/mapCtrl.js
+++ fb_test2/www/js/mapCtrl.js
@@ -1,11 +1,11 @@
 
 angular.module('starter.mapCtrl', [] )
 
-    .controller('mapCtrl', function($scope, $ionicLoading, $compile,$state,$cordovaGeolocation,$ionicPopup, $timeout , myFactoryService) 
-    {
-      $scope.rslt = myFactoryService.getData();
-      console.log('currentuser', $scope.rslt);
-      var options = {timeout: 10000, enableHighAccuracy: true};
+.controller('mapCtrl', function($scope, $ionicLoading, $compile,$state,$cordovaGeolocation,$ionicPopup, $timeout , myFactoryService) {
+  
+  $scope.rslt = myFactoryService.getData();
+  console.log('currentuser', $scope.rslt);
+  var options = {timeout: 10000, enableHighAccuracy: true};
  
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
     var latLng = new google.maps.LatLng(24.969417,121.267472);   
@@ -22,11 +22,12 @@ angular.module('starter.mapCtrl', [] )
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: "This is a marker!",
-    animation: google.maps.Animation.DROP
-  });
+      position: myLatLng,
+      map: map,
+      title: "This is a marker!",
+      animation: google.maps.Animation.DROP
+    });
+    
     $scope.map = map;
   
   }, function(error){
@@ -34,51 +35,51 @@ angular.module('starter.mapCtrl', [] )
   });
 
   $scope.centerOnYZU = function() {
-        if(!$scope.map) {
-          return;
-        }
+    if(!$scope.map) {
+      return;
+    }
 
-        $scope.loading = $ionicLoading.show({
-          content: 'Getting current location...',
-          showBackdrop: false
-        });
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
 
-        navigator.geolocation.getCurrentPosition(function(pos) {
-          $scope.map.setCenter(new google.maps.LatLng(24.969417,121.267472));
-          $scope.loading.hide();
-        }, function(error) {
-          alert('Unable to get location: ' + error.message);
-        });
-   };
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      $scope.map.setCenter(new google.maps.LatLng(24.969417,121.267472));
+      $scope.loading.hide();
+    }, function(error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
 
   $scope.centerOnMe = function() {
-        if(!$scope.map) {
-          return;
-        }
+    if(!$scope.map) {
+      return;
+    }
 
-        $scope.loading = $ionicLoading.show({
-          content: 'Getting current location...',
-          showBackdrop: false
-        });
-        navigator.geolocation.getCurrentPosition(function(pos) {
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+    navigator.geolocation.getCurrentPosition(function(pos) {
           
-          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
 
-          $scope.loading.hide();
-        }, function(error) {
-          alert('Unable to get location: ' + error.message);
-        });
-   };
+      $scope.loading.hide();
+    }, function(error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
 
   $scope.showAlert = function() {
-   var alertPopup = $ionicPopup.alert({
+    var alertPopup = $ionicPopup.alert({
       title: 'Ionic Popup',
       template: 'This is alert popup',
-   });
+    });
 
-   alertPopup.then(function(res) {
+    alertPopup.then(function(res) {
       console.log('Thanks');
-   });
+    });
   };
 
   $scope.showPopup = function() {
@@ -90,55 +91,58 @@ angular.module('starter.mapCtrl', [] )
       alert('Unable to get location: ' + error.message);
     });
 
-   $scope.data = {}
+    $scope.data = {}
    // An elaborate, custom popup
-   $ionicPopup.show({
-              templateUrl: 'popup-template.html',
-              title: '新增訊息',
-              cssClass: 'newMessage',
-              scope: $scope,
-              buttons: [
-                { text: '取消'},
-                {
-                  text: '<b>送出</b>',
-                  type: 'button-positive',
-                  onTap: function(e) {
-                    return $scope.data.message;
-                  }
-                },
-              ]
-              }).then(function(res) {
+    $ionicPopup.show({
+      templateUrl: 'popup-template.html',
+      title: '新增訊息',
+      cssClass: 'newMessage',
+      scope: $scope,
+      buttons: [
+        { text: '取消'},
+        {
+          text: '<b>送出</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            return $scope.data.message;
+          }
+        },
+      ]
+    }).then(function(res) {
 
-                var MessageObject = Parse.Object.extend("MessageObject");
-                var message = new MessageObject();
-                message.set("position",point);
-                message.set("username", $scope.rslt);
-                message.set("message", res);
-                message.save(null, {
-                  success: function (result){alert("Success")},error: function (error){alert("ERROR")} 
-                });
-                console.log('message', res);
-                alert(res);
-              });
+      var MessageObject = Parse.Object.extend("MessageObject");
+      var message = new MessageObject();
+      message.set("position",point);
+      message.set("username", $scope.rslt);
+      message.set("message", res);
+      message.save(null, {
+        success: function (result){
+          alert("Success")
+        },error: function (error){
+          alert("ERROR")} 
+        });
+        console.log('message', res);
+        alert(res);
+    });
   };
      
   $scope.value = true; // position toggle button
   $scope.positionChange = function()
   {
-      if($scope.value == false){
-        $scope.value = true;
-      }else
-        $scope.value = false;
-        alert('toggle: '+$scope.value);
+    if($scope.value == false){
+      $scope.value = true;
+    }else
+      $scope.value = false;
+      alert('toggle: '+$scope.value);
   };  
 
-   $scope.active = 'group';
-    $scope.setActive = function(type) {
-        alert(type);
-        $scope.active = type;
-    };
-    $scope.isActive = function(type) {
-        return type === $scope.active;
-    };  
+  $scope.active = 'group';
+  $scope.setActive = function(type) {
+    alert(type);
+    $scope.active = type;
+  };
+  $scope.isActive = function(type) {
+    return type === $scope.active;
+  };  
 
 });
