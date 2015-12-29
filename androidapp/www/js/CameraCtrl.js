902b621fde6001d7f63f48bcb2009772a375425f
AnitaTsai <momo19940901@gmail.com>
2015-12-30 00:23:08 +0800

    Camera

--- androidapp/www/js/CameraCtrl.js
+++ androidapp/www/js/CameraCtrl.js
@@ -2,19 +2,21 @@ angular.module('starter.CameraCtrl', ['ionic','ngCordova'] )
 
  .controller('CameraCtrl', function($scope, $cordovaCamera) 
  {
- 	$scope.pictureUrl = 'http://placehold.it/300x300';
+ 	$scope.imgURI = 'http://placehold.it/300x300';
    $scope.takePhoto = function () {
-   	alert("takePhoto");
                   var options = {
                     destinationType: Camera.DestinationType.DATA_URL,
+                    allowEdit: true,
+                    targetWidth: 300,
+                    targetHeight: 300,
                     encodingType: Camera.EncodingType.JPEG
                 };
    
                    $cordovaCamera.getPicture(options)
                    .then(function (data) {
-                    	alert("getPicture");
+                    //	alert("getPicture");
                     	console.log('camera data: ' + angular.toJson(data));
-                        $scope.pictureUrl = "data:image/jpeg;base64," + data;
+                        $scope.imgURI = "data:image/jpeg;base64," + data;
                     }, function (err) {
                         // An error occured. Show a message to the user
                         alert("error");
