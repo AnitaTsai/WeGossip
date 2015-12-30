d8a0ea37063e0d0a43fbb8527a4838bf614dfc80
AnitaTsai <momo19940901@gmail.com>
2015-12-31 03:16:50 +0800

    deleteProfilePic

--- androidapp/www/js/ProfileCtrl.js
+++ androidapp/www/js/ProfileCtrl.js
@@ -17,19 +17,6 @@ angular.module('starter.ProfileCtrl', ['ionic'] )
 	      }
 	    });
 
- 	var PictureObject = Parse.Object.extend("ProfilePictures");
- 	var query2 = new  Parse.Query(PictureObject);
- 	query2.equalTo("username", currentUser);
- 	query2.find({
-	      success: function(results) {
-	      //console.log(results[0].get('profilepic')['_url']);	          
-	          $scope.imgURI = results[0].get('profilepic')['_url'];
-	        },
-	      error: function(error) {
-	       alert("Error: " + error.code + " " + error.message);
-	      }
-	    });
-
  	$scope.SaveData = function()
  	{
  		
